5e0abb3bdedf64aedda6fa9c7b0c2bdb7f2f156b
Valentin Kolesnikov <javadev75@gmail.com>
2013-01-05 17:11:46 +0200

    Improve C# converter

--- src/main/cs/MoneyToStr.cs
+++ src/main/cs/MoneyToStr.cs
@@ -435,10 +435,182 @@ const string json = @"{
         this.kopFiveUnit = theISOElement["-KopFiveUnit"];
         this.rubSex = theISOElement["-RubSex"];
         this.kopSex = theISOElement["-KopSex"];
-        System.Console.WriteLine(this.rubSex);
+    }
+
+    /**
+     * Converts number to currency. Usage: MoneyToStr moneyToStr = new MoneyToStr("UAH"); String result =
+     * moneyToStr.convert(123D); Expected: result = сто двадцять три гривні 00 копійок
+     *
+     * @param theMoney
+     *            the amount of money major currency
+     * @param theKopeiki
+     *            the amount of money minor currency
+     * @return the string description of money value
+     */
+    public string convert(long theMoney, long theKopeiki) {
+        StringBuilder money2str = new StringBuilder();
+        long triadNum = 0;
+        long theTriad;
+
+        long intPart = theMoney;
+        if (intPart == 0) {
+            money2str.Append(messages["0"][0] + " ");
+        }
+        do {
+            theTriad = intPart % NUM1000;
+            money2str.Insert(0, triad2Word(theTriad, triadNum, rubSex));
+            if (triadNum == 0) {
+                long range10 = (theTriad % NUM100) / NUM10;
+                long range = theTriad % NUM10;
+                if (range10 == NUM1) {
+                    money2str.Append(rubFiveUnit);
+                } else {
+                    switch (range) {
+                    case NUM1:
+                        money2str.Append(rubOneUnit);
+                        break;
+                    case NUM2: case NUM3: case NUM4:
+                        money2str.Append(rubTwoUnit);
+                        break;
+                    default:
+                        money2str.Append(rubFiveUnit);
+                        break;
+                    }
+                }
+            }
+            intPart = intPart / NUM1000;
+            triadNum += 1;
+        } while (intPart > 0);
+
+        if (pennies == "TEXT") {
+            money2str.Append(language == "ENG" ? " and " : " ").Append(theKopeiki == 0 ? messages["0"][0] + " " : triad2Word(theKopeiki, 0, kopSex));
+        } else {
+            money2str.Append(" " + (theKopeiki < 10 ? "0" + Convert.ToString(theKopeiki) : Convert.ToString(theKopeiki)) + " ");
+        }
+        if (theKopeiki == NUM11 || theKopeiki == NUM12) {
+            money2str.Append(kopFiveUnit);
+        }
+        switch (theKopeiki % NUM10) {
+            case NUM1:
+                money2str.Append(kopOneUnit);
+                break;
+            case NUM2: case NUM3: case NUM4:
+                money2str.Append(kopTwoUnit);
+                break;
+            default:
+                money2str.Append(kopFiveUnit);
+                break;
+        }
+        return money2str.ToString().Trim();
+    }
+
+    private string triad2Word(long triad, long triadNum, string sex) {
+        StringBuilder triadWord = new StringBuilder();
+
+        if (triad == 0) {
+            return "";
+        }
+
+        long range = check1(triad, triadWord);
+        if (language == "ENG" && triadWord.Length > 0 && triad % NUM10 == 0) {
+            triadWord.Clear().Append(triadWord.ToString(0, triadWord.Length - 1));
+            triadWord.Append(" ");
+        }
+
+        long range10 = range;
+        range = triad % NUM10;
+        check2(triadNum, sex, triadWord, triad, range10);
+        switch (triadNum) {
+        case NUM0:
+            break;
+        case NUM1: case NUM2: case NUM3: case NUM4:
+            if (range10 == NUM1) {
+                triadWord.Append(messages["1000_10"][triadNum - 1] + " ");
+            } else {
+                switch (range) {
+                case NUM1:
+                    triadWord.Append(messages["1000_1"][triadNum - 1] + " ");
+                    break;
+                case NUM2: case NUM3: case NUM4:
+                    triadWord.Append(messages["1000_234"][triadNum - 1] + " ");
+                    break;
+                default:
+                    triadWord.Append(messages["1000_5"][triadNum - 1] + " ");
+                    break;
+                }
+            }
+            break;
+        default:
+            triadWord.Append("??? ");
+            break;
+        }
+        return triadWord.ToString();
+    }
+
+    /**
+     * @param triadNum the triad num
+     * @param sex the sex
+     * @param triadWord the triad word
+     * @param triad the triad
+     * @param range10 the range 10
+     */
+    private void check2(long triadNum, string sex, StringBuilder triadWord, long triad, long range10) {
+        long range = triad % NUM10;
+        if (range10 == 1) {
+            triadWord.Append(messages["10_19"][range] + " ");
+        } else {
+            switch (range) {
+            case NUM1:
+                if (triadNum == NUM1) {
+                    triadWord.Append(messages["1"][INDEX_0] + " ");
+                } else if (triadNum == NUM2 || triadNum == NUM3 || triadNum == NUM4) {
+                    triadWord.Append(messages["1"][INDEX_1] + " ");
+                } else if ("M" == sex) {
+                    triadWord.Append(messages["1"][INDEX_2] + " ");
+                } else if ("F" == sex) {
+                    triadWord.Append(messages["1"][INDEX_3] + " ");
+                }
+                break;
+            case NUM2:
+                if (triadNum == NUM1) {
+                    triadWord.Append(messages["2"][INDEX_0] + " ");
+                } else if (triadNum == NUM2 || triadNum == NUM3 || triadNum == NUM4) {
+                    triadWord.Append(messages["2"][INDEX_1] + " ");
+                } else if ("M" == sex) {
+                    triadWord.Append(messages["2"][INDEX_2] + " ");
+                } else if ("F" == sex) {
+                    triadWord.Append(messages["2"][INDEX_3] + " ");
+                }
+                break;
+            case NUM3: case NUM4: case NUM5: case NUM6: case NUM7: case NUM8: case NUM9:
+                triadWord.Append(concat(new string[] {"", "", ""}, messages["3_9"])[range] + " ");
+                break;
+            }
+        }
+    }
+
+    /**
+     * @param triad the triad
+     * @param triadWord the triad word
+     * @return the range
+     */
+    private long check1(long triad, StringBuilder triadWord) {
+        long range = triad / NUM100;
+        triadWord.Append(concat(new string[] {""}, messages["100_900"])[range]);
+
+        range = (triad % NUM100) / NUM10;
+        triadWord.Append(concat(new string[] {"", ""}, messages["20_90"])[range]);
+        return range;
+    }
+
+    private string[] concat(string[] first, string[] second) {
+        string[] result = new string[first.Length + second.Length];
+        first.CopyTo(result, 0);
+        second.CopyTo(result, first.Length);
+        return result;
     }
 
     static void Main(string[] args) {
-        System.Console.WriteLine(new MoneyToStr("RUR", "RUS", "TEXT"));
+        System.Console.WriteLine(new MoneyToStr("RUR", "RUS", "TEXT").convert(123, 34));
     }
 }
