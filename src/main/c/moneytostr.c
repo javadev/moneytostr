b90456cb41be5e6a3f8f3f6489785349ce70f688
Valentyn Kolesnikov <valentyn.kolesnikov@kruschecompany.com>
2013-09-04 10:26:49 +0300

    Added BCC 5.5 compiller installation

--- src/main/c/moneytostr.c
+++ src/main/c/moneytostr.c
@@ -364,5 +364,8 @@ char * convert(long rub, long kop) {
 }
 
 void main(int argc, char *argv[]) {
-printf("%s%s", convert(123, 45), json);
+  json_object *new_obj;
+  printf("%s%s", convert(123, 45), json);
+  new_obj = json_tokener_parse("/* more difficult test case */ { \"glossary\": { \"title\": \"example glossary\", \"GlossDiv\": { \"title\": \"S\", \"GlossList\": [ { \"ID\": \"SGML\", \"SortAs\": \"SGML\", \"GlossTerm\": \"Standard Generalized Markup Language\", \"Acronym\": \"SGML\", \"Abbrev\": \"ISO 8879:1986\", \"GlossDef\": \"A meta-markup language, used to create markup languages such as DocBook.\", \"GlossSeeAlso\": [\"GML\", \"XML\", \"markup\"] } ] } } }");
+  printf("new_obj.to_string()=%s\n", json_object_to_json_string(new_obj));
 }
