f774f052d73d0c81315c7596d780cf39a31d96b7
javadev <javadev75@gmail.com>
2014-07-19 10:59:02 +0300

    Improve cpp converter.

--- src/main/cpp/MoneyToStr.cpp
+++ src/main/cpp/MoneyToStr.cpp
@@ -3,6 +3,41 @@
 #include <string>
 using namespace std;
 
+class StringBuilder {
+    vector<string> _buffer;
+
+    StringBuilder() {
+//        this._buffer = new List();
+    }
+
+    public:
+    StringBuilder append(string text) {
+        _buffer.push_back(text);
+        return *this;
+    }
+/*
+    StringBuilder insert(index, text) {
+        _buffer.insert(index, text);
+        return *this;
+    }
+
+    int length() {
+        return toString().length();
+    }
+
+    StringBuilder deleteCharAt(index) {
+        var str = this.toString();
+        this._buffer = new List();
+        append(str.substring(0, index));
+        return this;
+    }
+
+    String toString() {
+        return _buffer.join("");
+    }
+*/
+};
+
 int main(){
     vector<string> strings;
     strings.push_back("string 1");
---
+++ src/main/cpp/compile.cmd
@@ -0,0 +1,1 @@
+bcc32 -f- -Og -d -vi- -wpro -weas -wpre -DWIN32 -IC:\Borland\BCC55\Include -LC:\Borland\BCC55\Lib MoneyToStr.cpp
