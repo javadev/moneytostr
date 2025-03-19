299f76c5857dc0718925dc0b922f45db71d8d4b7
Valentyn Kolesnikov <javadev75@gmail.com>
2025-03-19 09:09:01 +0200

    Changed year in copyright

--- src/main/java/com/github/moneytostr/MoneyToStr.java
+++ src/main/java/com/github/moneytostr/MoneyToStr.java
@@ -1,7 +1,7 @@
 /*
  * $Id$
  *
- * Copyright 2014-2024 Valentyn Kolesnikov
+ * Copyright 2014-2025 Valentyn Kolesnikov
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
